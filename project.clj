e37fec1a3f2fab9467da2dcd9c089696c936cbd8
Alex ter Weele <alexterweele@centripetalnetworks.com>
2017-05-26 11:50:57 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,21 +3,23 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [compojure "1.5.1"]
+                 ;; Duct framework and general web application
+                 ;; dependencies
+                 [com.stuartsierra/component "0.3.2"]
+                 [compojure "1.6.0"]
                  [duct "0.8.2"]
                  [environ "1.1.0"]
-                 [ring "1.5.0"]
-                 [ring/ring-defaults "0.2.1"]
+                 [ring "1.6.1"]
+                 [ring/ring-defaults "0.3.0"]
+                 [ring/ring-jetty-adapter "1.6.1"]
                  [ring/ring-json "0.4.0"]
                  [ring-jetty-component "0.3.1"]
-                 ;; TODO: this is not the latest version but to get
-                 ;; 0.9.0 I need to also upgrade ring.
-                 [metosin/ring-http-response "0.8.0"]
+                 [metosin/ring-http-response "0.9.0"]
                  ;; file parsing
-                 [org.clojure/data.csv "0.1.3"]
+                 [org.clojure/data.csv "0.1.4"]
                  ;; command-line argument parsing
                  [org.clojure/tools.cli "0.3.5"]
+                 ;; serializing to/from JSON
                  [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-environ "1.0.3"]
             [lein-cloverage "1.0.9"]]
@@ -33,8 +35,8 @@
    :project/dev   {:dependencies [[duct/generate "0.8.2"]
                                   [reloaded.repl "0.2.3"]
                                   [org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/tools.nrepl "0.2.12"]
-                                  [eftest "0.1.1"]
+                                  [org.clojure/tools.nrepl "0.2.13"]
+                                  [eftest "0.3.1"]
                                   [com.gearswithingears/shrubbery "0.4.1"]
                                   [kerodon "0.8.0"]
                                   [org.clojure/test.check "0.9.0"]]
